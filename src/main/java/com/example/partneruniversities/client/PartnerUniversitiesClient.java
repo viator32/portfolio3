8b1de2357186d239703c65452ff2dac951aae071
serhiiradkovskyi <sergii2003rad@gmail.com>
2024-06-10 21:22:32 +0200

    Fixed HATEOAS

--- src/main/java/com/example/partneruniversities/client/PartnerUniversitiesClient.java
+++ src/main/java/com/example/partneruniversities/client/PartnerUniversitiesClient.java
@@ -7,9 +7,12 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.client.Traverson;
 import org.springframework.hateoas.mediatype.hal.Jackson2HalModule;
+import org.springframework.hateoas.server.core.TypeReferences;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PartnerUniversitiesClient {
 
@@ -17,49 +20,64 @@ public class PartnerUniversitiesClient {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
+    private final Traverson traverson;
+    private final Map<String, URI> linkCache;
 
     public PartnerUniversitiesClient() {
         this.restTemplate = new RestTemplate();
-        Traverson traverson = new Traverson(URI.create(DISPATCHER_URL), MediaTypes.HAL_JSON);
-        traverson.setRestOperations(restTemplate);
+        this.traverson = new Traverson(URI.create(DISPATCHER_URL), MediaTypes.HAL_JSON);
+        this.traverson.setRestOperations(restTemplate);
         this.objectMapper = new ObjectMapper().registerModule(new Jackson2HalModule());
+        this.linkCache = new HashMap<>();
+        initializeLinks();
+    }
+
+    private void initializeLinks() {
+        linkCache.put("universities", URI.create(traverson.follow("universities").asLink().getHref()));
+        linkCache.put("modules", URI.create(traverson.follow("modules").asLink().getHref()));
     }
 
     public University createUniversity(University university) {
         EntityModel<University> universityEntityModel = restTemplate.postForObject(
-                DISPATCHER_URL + "universities", university, EntityModel.class);
+                linkCache.get("universities"), university, EntityModel.class);
         return objectMapper.convertValue(universityEntityModel.getContent(), University.class);
     }
 
     public Module createModule(Module module) {
         EntityModel<Module> moduleEntityModel = restTemplate.postForObject(
-                DISPATCHER_URL + "modules", module, EntityModel.class);
+                linkCache.get("modules"), module, EntityModel.class);
         return objectMapper.convertValue(moduleEntityModel.getContent(), Module.class);
     }
 
     public University updateUniversity(Long id, University university) {
-        restTemplate.put(DISPATCHER_URL + "universities/" + id, university);
-        return restTemplate.getForObject(DISPATCHER_URL + "universities/" + id, University.class);
+        URI universityUri = URI.create(linkCache.get("universities") + "/" + id);
+        restTemplate.put(universityUri, university);
+        return restTemplate.getForObject(universityUri, University.class);
     }
 
     public Module updateModule(Long id, Module module) {
-        restTemplate.put(DISPATCHER_URL + "modules/" + id, module);
-        return restTemplate.getForObject(DISPATCHER_URL + "modules/" + id, Module.class);
+        URI moduleUri = URI.create(linkCache.get("modules") + "/" + id);
+        restTemplate.put(moduleUri, module);
+        return restTemplate.getForObject(moduleUri, Module.class);
     }
 
     public void deleteUniversity(Long id) {
-        restTemplate.delete(DISPATCHER_URL + "universities/" + id);
+        URI universityUri = URI.create(linkCache.get("universities") + "/" + id);
+        restTemplate.delete(universityUri);
     }
 
     public void deleteModule(Long id) {
-        restTemplate.delete(DISPATCHER_URL + "modules/" + id);
+        URI moduleUri = URI.create(linkCache.get("modules") + "/" + id);
+        restTemplate.delete(moduleUri);
     }
 
     public University getUniversityById(Long id) {
-        return restTemplate.getForObject(DISPATCHER_URL + "universities/" + id, University.class);
+        URI universityUri = URI.create(linkCache.get("universities") + "/" + id);
+        return restTemplate.getForObject(universityUri, University.class);
     }
 
     public Module getModuleById(Long id) {
-        return restTemplate.getForObject(DISPATCHER_URL + "modules/" + id, Module.class);
+        URI moduleUri = URI.create(linkCache.get("modules") + "/" + id);
+        return restTemplate.getForObject(moduleUri, Module.class);
     }
 }
