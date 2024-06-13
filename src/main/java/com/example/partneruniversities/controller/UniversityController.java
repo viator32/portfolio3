9ca8c8fcc6e9c5b6254ec9330d83197b22d5f09d
serhiiradkovskyi <sergii2003rad@gmail.com>
2024-06-13 18:58:40 +0200

    Added search link to the header

--- src/main/java/com/example/partneruniversities/controller/UniversityController.java
+++ src/main/java/com/example/partneruniversities/controller/UniversityController.java
@@ -65,6 +65,7 @@ public class UniversityController {
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("self", linkTo(methodOn(UniversityController.class).getAllUniversities(page, size)).withSelfRel().getHref());
+        headers.add("search", linkTo(methodOn(UniversityController.class).searchUniversities("", "", "", 0, 5, "name", "asc")).withRel("search").getHref());
 
         return ResponseEntity.ok().headers(headers).body(collectionModel);
     }
